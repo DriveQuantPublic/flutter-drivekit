45c3b5e8bd13f376f4f49950c6a939a5d83af7f2
giboin <louisg@bam.tech>
2024-07-12 18:06:22 +0200

    feat(core android): use generated platform channel on dart side

--- packages/drivekit_core/drivekit_core_android/test/drivekit_core_android_test.dart
+++ packages/drivekit_core/drivekit_core_android/test/drivekit_core_android_test.dart
@@ -1,45 +1,26 @@
 import 'package:drivekit_core_android/drivekit_core_android.dart';
 import 'package:drivekit_core_platform_interface/drivekit_core_platform_interface.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mocks/mocks.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
   group('DrivekitCoreAndroid', () {
-    const kPlatformName = 'Android';
-    late DrivekitCoreAndroid drivekitCore;
-    late List<MethodCall> log;
-
-    setUp(() async {
-      drivekitCore = DrivekitCoreAndroid();
-
-      log = <MethodCall>[];
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler(drivekitCore.methodChannel,
-              (methodCall) async {
-        log.add(methodCall);
-        switch (methodCall.method) {
-          case 'getPlatformName':
-            return kPlatformName;
-          default:
-            return null;
-        }
-      });
-    });
-
     test('can be registered', () {
       DrivekitCoreAndroid.registerWith();
       expect(DrivekitCorePlatform.instance, isA<DrivekitCoreAndroid>());
     });
 
     test('getPlatformName returns correct name', () async {
+      //mock
+      final androidCoreApi = MockAndroidCoreApi();
+      when(androidCoreApi.getPlatformName).thenAnswer((_) async => 'Android');
+
+      //test
+      final drivekitCore = DrivekitCoreAndroid(androidCoreApi: androidCoreApi);
       final name = await drivekitCore.getPlatformName();
-      expect(
-        log,
-        <Matcher>[isMethodCall('getPlatformName', arguments: null)],
-      );
-      expect(name, equals(kPlatformName));
+      expect(name, 'Android');
     });
   });
 }
---
+++ packages/drivekit_core/drivekit_core_android/test/mocks/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:drivekit_core_android/src/core_api.g.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAndroidCoreApi extends Mock implements AndroidCoreApi {}
