45663c3a7d167b244edb98ffc65b7f759d06131a
giboin <louisg@bam.tech>
2024-07-15 15:29:04 +0200

    feat(trip_analysis): use pigeon method channel on dart side

--- packages/drivekit_trip_analysis/drivekit_trip_analysis_android/test/drivekit_trip_analysis_android_test.dart
+++ packages/drivekit_trip_analysis/drivekit_trip_analysis_android/test/drivekit_trip_analysis_android_test.dart
@@ -1,33 +1,14 @@
 import 'package:drivekit_trip_analysis_android/drivekit_trip_analysis_android.dart';
 import 'package:drivekit_trip_analysis_platform_interface/drivekit_trip_analysis_platform_interface.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mocks/mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group('DrivekitTripAnalysisAndroid', () {
-    const kPlatformName = 'Android';
-    late DrivekitTripAnalysisAndroid drivekitTripAnalysis;
-    late List<MethodCall> log;
-
-    setUp(() async {
-      drivekitTripAnalysis = DrivekitTripAnalysisAndroid();
-
-      log = <MethodCall>[];
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler(drivekitTripAnalysis.methodChannel,
-              (methodCall) async {
-        log.add(methodCall);
-        switch (methodCall.method) {
-          case 'getPlatformName':
-            return kPlatformName;
-          default:
-            return null;
-        }
-      });
-    });
-
     test('can be registered', () {
       DrivekitTripAnalysisAndroid.registerWith();
       expect(
@@ -37,12 +18,17 @@ void main() {
     });
 
     test('getPlatformName returns correct name', () async {
-      final name = await drivekitTripAnalysis.getPlatformName();
-      expect(
-        log,
-        <Matcher>[isMethodCall('getPlatformName', arguments: null)],
+      //mocks
+      final androidTripAnalysisApi = MockAndroidTripAnalysisApi();
+      when(androidTripAnalysisApi.getPlatformName)
+          .thenAnswer((_) async => 'Android');
+
+      //test
+      final drivekitTripAnalysisAndroid = DrivekitTripAnalysisAndroid(
+        androidTripAnalysisApi: androidTripAnalysisApi,
       );
-      expect(name, equals(kPlatformName));
+      final name = await drivekitTripAnalysisAndroid.getPlatformName();
+      expect(name, 'Android');
     });
   });
 }
--- packages/drivekit_trip_analysis/drivekit_trip_analysis_ios/test/drivekit_trip_analysis_ios_test.dart
+++ packages/drivekit_trip_analysis/drivekit_trip_analysis_ios/test/drivekit_trip_analysis_ios_test.dart
@@ -1,33 +1,14 @@
 import 'package:drivekit_trip_analysis_ios/drivekit_trip_analysis_ios.dart';
 import 'package:drivekit_trip_analysis_platform_interface/drivekit_trip_analysis_platform_interface.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import 'mocks/mocks.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group('DrivekitTripAnalysisIOS', () {
-    const kPlatformName = 'iOS';
-    late DrivekitTripAnalysisIOS drivekitTripAnalysis;
-    late List<MethodCall> log;
-
-    setUp(() async {
-      drivekitTripAnalysis = DrivekitTripAnalysisIOS();
-
-      log = <MethodCall>[];
-      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
-          .setMockMethodCallHandler(drivekitTripAnalysis.methodChannel,
-              (methodCall) async {
-        log.add(methodCall);
-        switch (methodCall.method) {
-          case 'getPlatformName':
-            return kPlatformName;
-          default:
-            return null;
-        }
-      });
-    });
-
     test('can be registered', () {
       DrivekitTripAnalysisIOS.registerWith();
       expect(
@@ -37,12 +18,15 @@ void main() {
     });
 
     test('getPlatformName returns correct name', () async {
-      final name = await drivekitTripAnalysis.getPlatformName();
-      expect(
-        log,
-        <Matcher>[isMethodCall('getPlatformName', arguments: null)],
-      );
-      expect(name, equals(kPlatformName));
+      //mocks
+      final iOSTripAnalysisApi = MockIOSTripAnalysisApi();
+      when(iOSTripAnalysisApi.getPlatformName).thenAnswer((_) async => 'iOS');
+
+      //test
+      final drivekitTripAnalysisIOS =
+          DrivekitTripAnalysisIOS(iosTripAnalysisApi: iOSTripAnalysisApi);
+      final name = await drivekitTripAnalysisIOS.getPlatformName();
+      expect(name, 'iOS');
     });
   });
 }
