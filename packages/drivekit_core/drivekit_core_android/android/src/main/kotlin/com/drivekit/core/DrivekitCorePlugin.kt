076b06fe536b283c5235de94f353a72d5ced3ce8
giboin <louisg@bam.tech>
2024-07-15 11:22:51 +0200

    feat(core android): plug the native side to pigeon output

---
+++ packages/drivekit_core/drivekit_core_android/android/src/main/kotlin/com/drivekit/core/DrivekitCorePlugin.kt
@@ -0,0 +1,25 @@
+package com.drivekit.core
+
+import AndroidCoreApi
+import android.content.Context
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+class DrivekitCorePlugin : FlutterPlugin, AndroidCoreApi {
+    private var context: Context? = null
+
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        AndroidCoreApi.setUp(flutterPluginBinding.binaryMessenger, this)
+        context = flutterPluginBinding.applicationContext
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        AndroidCoreApi.setUp(binding.binaryMessenger, null)
+        context = null
+    }
+
+    override fun getPlatformName(): String {
+        return "android"
+    }
+
+}
