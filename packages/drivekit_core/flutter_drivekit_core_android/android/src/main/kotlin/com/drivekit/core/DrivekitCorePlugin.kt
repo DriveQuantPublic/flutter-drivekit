ec46616cbcfd6a574736cfc25558c0b207b958ad
giboin <louisg@bam.tech>
2024-07-17 10:37:35 +0200

    feat(core android): add setUserId on native side

--- packages/drivekit_core/flutter_drivekit_core_android/android/src/main/kotlin/com/drivekit/core/DrivekitCorePlugin.kt
+++ packages/drivekit_core/flutter_drivekit_core_android/android/src/main/kotlin/com/drivekit/core/DrivekitCorePlugin.kt
@@ -27,4 +27,8 @@ class DrivekitCorePlugin : FlutterPlugin, AndroidCoreApi {
        DriveKit.setApiKey(key)
     }
 
+    override fun setUserId(userId:String) {
+        DriveKit.setUserId(userId)
+    }
+
 }
