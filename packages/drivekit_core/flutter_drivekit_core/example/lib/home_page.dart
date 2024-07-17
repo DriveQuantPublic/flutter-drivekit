71fe78505ebb2f7ac36db24e1504fae49426f296
giboin <louisg@bam.tech>
2024-07-17 10:39:51 +0200

    refactor(example): extract home page in its file

---
+++ packages/drivekit_core/flutter_drivekit_core/example/lib/home_page.dart
@@ -0,0 +1,33 @@
+import 'package:drivekit_core_example/widgets/sliver_api_key.dart';
+import 'package:drivekit_core_example/widgets/sliver_platform_name.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('DriveKit Core Example'),
+            pinned: true,
+          ),
+          SliverGap(32),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverPlatformName(),
+          ),
+          SliverGap(32),
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            sliver: SliverApiKey(),
+          ),
+          SliverGap(32),
+        ],
+      ),
+    );
+  }
+}
--- packages/drivekit_core/flutter_drivekit_core/example/lib/main.dart
+++ packages/drivekit_core/flutter_drivekit_core/example/lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:drivekit_core_example/widgets/sliver_api_key.dart';
-import 'package:drivekit_core_example/widgets/sliver_platform_name.dart';
+import 'package:drivekit_core_example/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,34 +9,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(home: HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            title: Text('DriveKit Core Example'),
-            pinned: true,
-          ),
-          SliverGap(32),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            sliver: SliverPlatformName(),
-          ),
-          SliverGap(32),
-          SliverPadding(
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            sliver: SliverApiKey(),
-          ),
-          SliverGap(32),
-        ],
-      ),
-    );
   }
 }
