2502175eee6753ac53ba646b556a132f2481cf21
giboin <louisg@bam.tech>
2024-07-17 17:52:58 +0200

    fix(trip_analysis): duplicated appId

--- packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis/example/android/app/src/main/kotlin/com/example/drivekit_trip_analysis/example/MainActivity.kt
+++ packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis/example/android/app/src/main/kotlin/com/example/drivekit_trip_analysis/example/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.verygoodcore.example
+package com.example.verygoodtrip.example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity() {}
