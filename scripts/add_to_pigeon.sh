0d911e32d7b300898f3828d00b3de4c3a56669d0
giboin <louisg@bam.tech>
2024-07-19 15:48:55 +0200

    fix pigeon script

--- scripts/add_to_pigeon.sh
+++ scripts/add_to_pigeon.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Script to copy the last method of PlatformInterface class from one Dart file to pigeon class in another Dart file
+# Script to copy the last method of PlatformInterface class from one Dart file to Pigeon class in another Dart file
 
 # Function to display usage information
 usage() {
     echo "Usage: $0 <platform_interface_file> <pigeon_file>"
     echo "  <platform_interface_file>: Path to the Dart file containing PlatformInterface class"
-    echo "  <pigeon_file>: Path to the Dart file containing DefaultImplementation class"
+    echo "  <pigeon_file>: Path to the Dart file containing Pigeon class"
     echo "  <platform_interface_class_name>: name of the class containing the platform interface"
-    echo "  <pigeon_class_name>: name of the class containing the DefaultImplementation"
+    echo "  <pigeon_class_name>: name of the class containing the Pigeon class"
     exit 1
 }
 
@@ -19,7 +19,6 @@ fi
 
 platform_interface_file="$1"
 pigeon_file="$2"
-
 platform_interface_class_name="$3"
 pigeon_class_name="$4"
 
@@ -49,12 +48,12 @@ if [ -z "$last_method" ]; then
     exit 1
 fi
 
-# Prepare the method for insertion without the trailing semicolon and any leading/trailing spaces
-escaped_method=$(echo "$last_method" | sed 's/[&/\]/\\&/g' | sed 's/;$//' | sed 's/^[ \t]*//' | sed 's/[ \t]*$//' | sed 's/}//g')
+# Extract return type (part before the first space character)
+return_type=$(echo "$last_method" | awk '{print $1}')
 
-# Extract return type and method signature
-return_type=$(echo "$escaped_method" | awk '{print $1}')
-method_signature=$(echo "$escaped_method" | cut -d' ' -f2-)
+# Remove the return type from the method signature
+method_name_and_arguments=$(echo "$last_method" | awk '{print substr($0, index($0,$2))}' | cut -d ';' -f 1)
+
 
 # Check if the return type is Future<Type>
 if [[ "$return_type" == Future* ]]; then
@@ -64,14 +63,14 @@ else
 fi
 
 # Prepare the final method for insertion
-final_method="$inner_type $method_signature"
+final_method="$inner_type $method_name_and_arguments;"
 
-# Insert the last method at the end of DefaultImplementation class
+# Insert the last method at the end of Pigeon class
 sed -i '' "/class $pigeon_class_name/,/^}/ {/^}/i\\
 $final_method
 }" "$pigeon_file"
 
-# Format the DefaultImplementation.dart file
+# Format the pigeon.dart file
 dart format "$pigeon_file"
 
 echo "Last method from $platform_interface_class_name has been copied to the end of $pigeon_class_name class in $pigeon_file and the file has been formatted."
