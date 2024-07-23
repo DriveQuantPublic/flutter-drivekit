63f73671f95e1545731caddd3e4f90a25d661595
Dennis BORDET <dennisb@bam.tech>
2024-07-23 11:37:10 +0200

    feat: add driveKitListener on initialization

--- packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/DrivekitTripAnalysisPlugin.kt
+++ packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/DrivekitTripAnalysisPlugin.kt
@@ -1,6 +1,12 @@
 package com.drivequant.drivekit.flutter.tripanalysis
 
 import android.content.Context
+import com.drivequant.drivekit.core.DriveKit
+import com.drivequant.drivekit.core.driver.UpdateUserIdStatus
+import com.drivequant.drivekit.core.driver.deletion.DeleteAccountStatus
+import com.drivequant.drivekit.core.networking.DriveKitListener
+import com.drivequant.drivekit.core.networking.RequestError
+import com.drivequant.drivekit.flutter.tripanalysis.mapper.PigeonMapper.toPigeon
 import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysis
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
@@ -9,6 +15,10 @@ class DrivekitTripAnalysisPlugin :
     AndroidTripAnalysisApi {
     private var context: Context? = null
     private var flutterApi: FlutterTripAnalysisApi? = null
+
+    init {
+        configureDriveKitListener()
+    }
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         AndroidTripAnalysisApi.setUp(flutterPluginBinding.binaryMessenger, this)
@@ -27,9 +37,33 @@ class DrivekitTripAnalysisPlugin :
         DriveKitTripAnalysis.activateAutoStart(activate)
     }
 
+    private fun configureDriveKitListener() {
+        DriveKit.addDriveKitListener(object : DriveKitListener {
+            override fun onAccountDeleted(status: DeleteAccountStatus) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onAuthenticationError(errorType: RequestError) {
+                this@DrivekitTripAnalysisPlugin.onAuthenticationError(errorType)
+            }
+
+            override fun onConnected() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDisconnected() {
+                TODO("Not yet implemented")
+            }
+
+            override fun userIdUpdateStatus(status: UpdateUserIdStatus, userId: String?) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+
     // example of how to call flutter from native, will be used with listener
-    private fun onAuthenticationError(requestError: PigeonRequestError) {
-        flutterApi?.onAuthenticationError(requestError) { echo ->
+    private fun onAuthenticationError(requestError: RequestError) {
+        flutterApi?.onAuthenticationError(requestError.toPigeon()) { echo ->
             Result.success(echo)
         }
     }
