75772cbe60e6d0978d3cf7f1849ac663c8909951
Dennis BORDET <dennisb@bam.tech>
2024-07-23 14:13:39 +0200

    feat: init FlutterTripAnalysisApi in swift

--- packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_ios/ios/Classes/DrivekitTripAnalysisPlugin.swift
+++ packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_ios/ios/Classes/DrivekitTripAnalysisPlugin.swift
@@ -5,11 +5,16 @@ import DriveKitTripAnalysisModule
 extension FlutterError: Error {}
 
 public class DrivekitTripAnalysisPlugin: NSObject, FlutterPlugin, IOSTripAnalysisApi {
+    var flutterAPI: FlutterTripAnalysisApi? = nil
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let messenger : FlutterBinaryMessenger = registrar.messenger()
-        let api : IOSTripAnalysisApi & NSObjectProtocol = DrivekitTripAnalysisPlugin.init()
-
+        let api : IOSTripAnalysisApi & NSObjectProtocol = DrivekitTripAnalysisPlugin.init(binaryMessenger: messenger )
         IOSTripAnalysisApiSetup.setUp(binaryMessenger: messenger, api: api);
+    }
+
+    init(binaryMessenger: FlutterBinaryMessenger) {
+        self.flutterAPI = FlutterTripAnalysisApi(binaryMessenger: binaryMessenger)
     }
 
     public func getPlatformName() throws -> String {
