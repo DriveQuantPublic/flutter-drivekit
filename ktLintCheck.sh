02c7dc077dd92f4ec78df4d386251c76570a1b02
Dennis BORDET <dennisb@bam.tech>
2024-07-23 16:13:49 +0200

    chore(linter): create bash script to run ktLint

---
+++ ktLintCheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Constant for the path to the gradlew script
+PROJECT_PATH="packages/drivekit_core/flutter_drivekit_core/example/android/"
+GRADLEW_PATH="$PROJECT_PATH/gradlew"
+
+# Check if the gradlew script exists at the specified path
+if [ ! -f "$GRADLEW_PATH" ]; then
+  echo "Error: $GRADLEW_PATH not found!"
+  exit 1
+fi
+
+# Run the gradle command with the specified task(s)
+cd "$PROJECT_PATH" || exit
+./gradlew :flutter_drivekit_trip_analysis_android:lintKotlin
+./gradlew :flutter_drivekit_core_android:lintKotlin
+
+# Check if the gradle command was successful
+if [ $? -eq 0 ]; then
+  echo "Gradle command executed successfully."
+else
+  echo "Gradle command failed."
+  exit 1
+fi
