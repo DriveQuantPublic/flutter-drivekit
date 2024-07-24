ca13a441d7e9dd5b1d1cb00748cfc08e4b110b73
Dennis BORDET <dennisb@bam.tech>
2024-07-24 14:09:16 +0200

    chore(linter): autocorrect when possible

--- scripts/swiftLint.sh
+++ scripts/swiftLint.sh
@@ -6,5 +6,5 @@ for PROJECT_NAME in "${PROJECT_NAMES[@]}"; do
   PROJECT_ROOT="packages/${PROJECT_NAME}/flutter_${PROJECT_NAME}_ios/ios"
   PODS_ROOT="packages/${PROJECT_NAME}/flutter_${PROJECT_NAME}/example/ios/Pods"
 
-  "$PODS_ROOT/SwiftLint/swiftlint" "$PROJECT_ROOT" --config "$PROJECT_ROOT/.swiftlint.yml"
+  "$PODS_ROOT/SwiftLint/swiftlint" "$PROJECT_ROOT" --fix --config "$PROJECT_ROOT/.swiftlint.yml"
 done
