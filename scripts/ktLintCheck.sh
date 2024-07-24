f8acc294931c1e8996bfcc9c594913bbbae5ff83
Dennis BORDET <dennisb@bam.tech>
2024-07-24 10:35:03 +0200

    refactor(ci): move ktlintScript in /scripts

--- scripts/ktLintCheck.sh
+++ scripts/ktLintCheck.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is configured to be launched with melos
+# melos analyzeNative
 
 # Constant for the path to the gradlew script
 PROJECT_PATH="packages/drivekit_core/flutter_drivekit_core/example/android"
