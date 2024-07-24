4f8f8b373e02cb2940937ca7ed4d01654aec988b
Dennis BORDET <dennisb@bam.tech>
2024-07-24 14:05:08 +0200

    chore(linter): script to run swiftlint in ios lib

---
+++ scripts/swiftLint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROJECT_NAMES=("drivekit_core")
+
+for PROJECT_NAME in "${PROJECT_NAMES[@]}"; do
+  PROJECT_ROOT="packages/${PROJECT_NAME}/flutter_${PROJECT_NAME}_ios/ios"
+  PODS_ROOT="packages/${PROJECT_NAME}/flutter_${PROJECT_NAME}/example/ios/Pods"
+
+  "$PODS_ROOT/SwiftLint/swiftlint" "$PROJECT_ROOT" --config "$PROJECT_ROOT/.swiftlint.yml"
+done
