41bf6ac835f9283c32c6161a02d6cffa4c4cfa0c
Dennis BORDET <dennisb@bam.tech>
2024-07-24 15:15:00 +0200

    chore(ci): add ktlint to pre-commit hook

---
+++ scripts/ktLintFormat.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# This script is configured to be launched from root
+
+# Constant for the path to the gradlew script
+PROJECT_PATH="packages/drivekit_core/flutter_drivekit_core/example/android"
+GRADLEW_PATH="$PROJECT_PATH/gradlew"
+
+# Check if the gradlew script exists at the specified path
+if [ ! -f "$GRADLEW_PATH" ]; then
+  echo "Error: $GRADLEW_PATH not found!"
+  exit 1
+fi
+
+# Run the gradle command with the specified task(s)
+cd "$PROJECT_PATH" || exit
+./gradlew :flutter_drivekit_trip_analysis_android:formatKotlin
+./gradlew :flutter_drivekit_core_android:formatKotlin
+
+# Check if the gradle command was successful
+if [ $? -eq 0 ]; then
+  echo "Gradle command executed successfully."
+else
+  echo "Gradle command failed."
+  exit 1
+fi
