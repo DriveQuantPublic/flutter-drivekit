7bb159863f0fc68164fc985d1be53924356b3229
Dennis BORDET <dennisb@bam.tech>
2024-07-25 11:22:04 +0200

    feat(vehicle): implement setVehicle in android

--- packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/DrivekitTripAnalysisPlugin.kt
+++ packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/DrivekitTripAnalysisPlugin.kt
@@ -1,6 +1,7 @@
 package com.drivequant.drivekit.flutter.tripanalysis
 
 import android.content.Context
+import com.drivequant.drivekit.flutter.tripanalysis.mapper.PigeonMapper
 import com.drivequant.drivekit.tripanalysis.DriveKitTripAnalysis
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 
@@ -41,4 +42,8 @@ class DrivekitTripAnalysisPlugin :
     }
 
     override fun isTripRunning(): Boolean = DriveKitTripAnalysis.isTripRunning()
+
+    override fun setVehicle(vehicle: PigeonVehicle) {
+        DriveKitTripAnalysis.setVehicle(PigeonMapper.fromPigeonVehicle(vehicle))
+    }
 }
