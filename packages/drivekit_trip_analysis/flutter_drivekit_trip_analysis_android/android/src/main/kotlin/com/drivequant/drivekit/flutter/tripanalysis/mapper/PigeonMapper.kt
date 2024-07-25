8e42fb45f26c7e799ebfd730ad283920a2a1ba9b
Dennis BORDET <dennisb@bam.tech>
2024-07-25 11:20:02 +0200

    feat(vehicle): create simple mapper

--- packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/mapper/PigeonMapper.kt
+++ packages/drivekit_trip_analysis/flutter_drivekit_trip_analysis_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripanalysis/mapper/PigeonMapper.kt
@@ -1,4 +1,28 @@
 // PigeonMapper object with the mapping function
 package com.drivequant.drivekit.flutter.tripanalysis.mapper
 
-object PigeonMapper
+import com.drivequant.drivekit.flutter.tripanalysis.PigeonVehicle
+import com.drivequant.drivekit.tripanalysis.entity.TripVehicle
+
+object PigeonMapper {
+    fun fromPigeonVehicle(pigeonVehicle: PigeonVehicle): TripVehicle = TripVehicle(
+        carTypeIndex = pigeonVehicle.carTypeIndex.toInt(),
+        carEngineIndex = pigeonVehicle.carEngineIndex.toInt(),
+        carPower = pigeonVehicle.carPower,
+        carMass = pigeonVehicle.carMass,
+        carGearboxIndex = pigeonVehicle.carGearboxIndex.toInt(),
+        carConsumption = pigeonVehicle.carConsumption,
+        carAutoGearboxNumber = pigeonVehicle.carAutoGearboxNumber.toInt(),
+        engineDisplacement = pigeonVehicle.engineDisplacement,
+        carPassengers = pigeonVehicle.carPassengers.toInt(),
+        dqIndex = pigeonVehicle.dqIndex,
+        sra = pigeonVehicle.sra,
+        frontTireSize = pigeonVehicle.frontTireSize,
+        rearTireSize = pigeonVehicle.rearTireSize,
+        length = pigeonVehicle.length,
+        width = pigeonVehicle.width,
+        height = pigeonVehicle.height,
+        engineCylinderNb = pigeonVehicle.engineCylinderNb?.toInt(),
+        driveWheels = pigeonVehicle.driveWheels?.toInt()
+    )
+}
