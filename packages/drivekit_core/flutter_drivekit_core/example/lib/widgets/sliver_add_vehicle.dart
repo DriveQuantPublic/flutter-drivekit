f66aa68fc0a57743232f2552047f45eee6ba60e8
Dennis BORDET <dennisb@bam.tech>
2024-07-25 15:53:32 +0200

    feat: set vehicle in example

---
+++ packages/drivekit_core/flutter_drivekit_core/example/lib/widgets/sliver_add_vehicle.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_drivekit_trip_analysis/flutter_drivekit_trip_analysis.dart';
+import 'package:flutter_drivekit_trip_analysis_platform_interface/src/model.dart';
+
+class SliverAddVehicle extends StatefulWidget {
+  const SliverAddVehicle({super.key});
+
+  @override
+  State<SliverAddVehicle> createState() => _SliverAddVehicleState();
+}
+
+class _SliverAddVehicleState extends State<SliverAddVehicle> {
+  @override
+  Widget build(BuildContext context) {
+    final driveKitTripAnalysis = DrivekitTripAnalysis();
+    return SliverToBoxAdapter(
+      child: ElevatedButton(
+        onPressed: () {
+          driveKitTripAnalysis.setVehicle(
+            const Vehicle(
+              carTypeIndex: 2,
+              carEngineIndex: 2,
+              carPower: 200,
+              carMass: 1500,
+              carGearboxIndex: 3,
+              carConsumption: 6.5,
+              carAutoGearboxNumber: 1,
+              engineDisplacement: 1500,
+              carPassengers: 2,
+              length: 4.7,
+              width: 1.9,
+              height: 1.5,
+              engineCylinderNb: 6,
+              driveWheels: 1,
+            ),
+          );
+        },
+        child: const Text('Add Default Vehicle'),
+      ),
+    );
+  }
+}
