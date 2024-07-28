623d318ace3305694087955309a7786ccdcaaecc
giboin <louisg@bam.tech>
2024-07-28 17:46:24 +0200

    feat(core interface): complete model

--- packages/drivekit_core/flutter_drivekit_core_platform_interface/lib/src/model.dart
+++ packages/drivekit_core/flutter_drivekit_core_platform_interface/lib/src/model.dart
@@ -1,8 +1,55 @@
 /// Listener on DriveKit core.
 class DriveKitListener {
   /// creates a [DriveKitListener]
-  DriveKitListener({this.onConnected});
+  DriveKitListener({
+    this.onConnected,
+    this.onDisconnected,
+    this.userIdUpdateStatus,
+    this.onAuthenticationError,
+    this.onAccountDeleted,
+    this.onBackgroundFetchStatusChanged,
+  });
 
   /// callback triggered when the user is connected
   void Function()? onConnected;
+  void Function()? onDisconnected;
+  void Function(
+    UpdateUserIdStatus status,
+    String? userId,
+  )? userIdUpdateStatus;
+  void Function(RequestError errorType)? onAuthenticationError;
+  void Function(DeleteAccountStatus status)? onAccountDeleted;
+
+  /// iOS only
+  void Function(BackgroundFetchStatus status)? onBackgroundFetchStatusChanged;
+}
+
+enum DeleteAccountStatus {
+  success,
+  failedToDelete,
+  forbidden;
+}
+
+enum RequestError {
+  wrongUrl,
+  noNetwork,
+  unauthenticated,
+  forbidden,
+  serverError,
+  clientError,
+  limitReached,
+  unknownError;
+}
+
+enum UpdateUserIdStatus {
+  updated,
+  failedToUpdate,
+  invalidUserId,
+  alreadyUsed,
+  savedForRepost;
+}
+
+enum BackgroundFetchStatus {
+  started,
+  completed;
 }
