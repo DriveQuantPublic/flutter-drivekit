d63507386a3cc414e1f3692353326497e763607f
giboin <louisg@bam.tech>
2024-07-30 18:20:36 +0200

    feat(example): remove listener on dispose

--- example/lib/widgets/initialize_listeners.dart
+++ example/lib/widgets/initialize_listeners.dart
@@ -11,67 +11,73 @@ class InitializeListeners extends StatefulWidget {
 }
 
 class _InitializeListenersState extends State<InitializeListeners> {
+  late final DriveKitListener _listener;
   @override
   void initState() {
     super.initState();
-    DriveKitCore.instance.addDriveKitListener(
-      DriveKitListener(
-        onConnected: () {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('DriveKit connected'),
-              ),
-            );
-          }
-        },
-        onDisconnected: () {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('DriveKit disconnected'),
-              ),
-            );
-          }
-        },
-        userIdUpdateStatus: (status, userId) {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('User ID update status: $status'),
-              ),
-            );
-          }
-        },
-        onAuthenticationError: (errorType) {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Authentication error: $errorType'),
-              ),
-            );
-          }
-        },
-        onAccountDeleted: (status) {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Account deletion status: $status'),
-              ),
-            );
-          }
-        },
-        onBackgroundFetchStatusChanged: (status) {
-          if (context.mounted) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Background fetch status: $status'),
-              ),
-            );
-          }
-        },
-      ),
+    _listener = DriveKitListener(
+      onConnected: () {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('DriveKit connected'),
+            ),
+          );
+        }
+      },
+      onDisconnected: () {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('DriveKit disconnected'),
+            ),
+          );
+        }
+      },
+      userIdUpdateStatus: (status, userId) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('User ID update status: $status'),
+            ),
+          );
+        }
+      },
+      onAuthenticationError: (errorType) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Authentication error: $errorType'),
+            ),
+          );
+        }
+      },
+      onAccountDeleted: (status) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Account deletion status: $status'),
+            ),
+          );
+        }
+      },
+      onBackgroundFetchStatusChanged: (status) {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Background fetch status: $status'),
+            ),
+          );
+        }
+      },
     );
+    DriveKitCore.instance.addDriveKitListener(_listener);
+  }
+
+  @override
+  void dispose() {
+    DriveKitCore.instance.removeDriveKitListener(_listener);
+    super.dispose();
   }
 
   @override
