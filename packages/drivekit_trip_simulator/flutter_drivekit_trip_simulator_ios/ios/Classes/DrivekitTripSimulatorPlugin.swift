eae81b7a5cb36a07564e3f0167207eecb8960b56
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2024-07-31 11:18:11 +0200

    chore(tripSimulator): add PigeonMapper for TripSimulator module

--- packages/drivekit_trip_simulator/flutter_drivekit_trip_simulator_ios/ios/Classes/DrivekitTripSimulatorPlugin.swift
+++ packages/drivekit_trip_simulator/flutter_drivekit_trip_simulator_ios/ios/Classes/DrivekitTripSimulatorPlugin.swift
@@ -17,33 +17,11 @@ public class DrivekitTripSimulatorPlugin: NSObject, FlutterPlugin, IOSTripSimula
     }
 
     func start(presetTrip: PigeonPresetTrip) throws {
-        switch presetTrip {
-            case .shortTrip:
-                DriveKitTripSimulator.shared.start(.shortTrip)
-            case .mixedTrip:
-                DriveKitTripSimulator.shared.start(.mixedTrip)
-            case .cityTrip:
-                DriveKitTripSimulator.shared.start(.cityTrip)
-            case .suburbanTrip:
-                DriveKitTripSimulator.shared.start(.suburbanTrip)
-            case .highwayTrip:
-                DriveKitTripSimulator.shared.start(.highwayTrip)
-            case .trainTrip:
-                DriveKitTripSimulator.shared.start(.trainTrip)
-            case .boatTrip:
-                DriveKitTripSimulator.shared.start(.boatTrip)
-            case .busTrip:
-                DriveKitTripSimulator.shared.start(.busTrip)
-            case .tripWithCrashConfirmed10KmH:
-                DriveKitTripSimulator.shared.startCrashTrip(.confirmed10KmH)
-            case .tripWithCrashConfirmed20KmH:
-                DriveKitTripSimulator.shared.startCrashTrip(.confirmed20KmH)
-            case .tripWithCrashConfirmed30KmH:
-                DriveKitTripSimulator.shared.startCrashTrip(.confirmed30KmH)
-            case .tripWithCrashUnconfirmed0KmH:
-                DriveKitTripSimulator.shared.startCrashTrip(.unconfirmed0KmH)
-            case .tripWithCrashConfirmed30KmHStillDriving:
-                DriveKitTripSimulator.shared.startCrashTrip(.confirmed30KmHStillDriving)
+        let (tripToSimulate, crashTripToSimulate) = PigeonMapper.getPresetTrip(from: presetTrip)
+        if let tripToSimulate {
+            DriveKitTripSimulator.shared.start(tripToSimulate)
+        } else if let crashTripToSimulate {
+            DriveKitTripSimulator.shared.startCrashTrip(crashTripToSimulate)
         }
     }
     
