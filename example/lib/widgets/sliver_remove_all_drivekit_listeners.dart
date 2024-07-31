5b4db22a459dfacfb81532e994d2c4f1b3bd8b59
giboin <louisg@bam.tech>
2024-07-31 12:42:09 +0200

    refactor(example): rename remove all listeners

--- example/lib/widgets/sliver_remove_all_drivekit_listeners.dart
+++ example/lib/widgets/sliver_remove_all_drivekit_listeners.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_drivekit_core/flutter_drivekit_core.dart';
 
-class SliverRemoveAllListeners extends StatelessWidget {
-  const SliverRemoveAllListeners({super.key});
+class SliverRemoveAllDriveKitListeners extends StatelessWidget {
+  const SliverRemoveAllDriveKitListeners({super.key});
 
   @override
   Widget build(BuildContext context) {
