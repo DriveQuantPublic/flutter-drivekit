c47b2416ab5f83b40d28d2693d86fa127ca4c7d5
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2024-07-31 11:33:20 +0200

    chore(tripSimulator): add TripSimulatorPigeonMapper for android

---
+++ packages/drivekit_trip_simulator/flutter_drivekit_trip_simulator_android/android/src/main/kotlin/com/drivequant/drivekit/flutter/tripsimulator/TripSimulatorPigeonMapper.kt
@@ -0,0 +1,23 @@
+package com.drivequant.drivekit.flutter.core.mapper
+
+import com.drivequant.drivekit.flutter.tripsimulator.PigeonPresetTrip
+import com.drivequant.drivekit.tripsimulator.PresetTrip
+import com.drivequant.drivekit.tripsimulator.model.PresetTripCrash1
+
+internal object TripSimulatorPigeonMapper {
+    fun fromPresetTrip(pigeonPresetTrip: PigeonPresetTrip): PresetTrip = when (pigeonPresetTrip) {
+        PigeonPresetTrip.SHORT_TRIP -> PresetTrip.SHORT_TRIP
+        PigeonPresetTrip.MIXED_TRIP -> PresetTrip.MIXED_TRIP
+        PigeonPresetTrip.CITY_TRIP -> PresetTrip.CITY_TRIP
+        PigeonPresetTrip.SUBURBAN_TRIP -> PresetTrip.SUBURBAN_TRIP
+        PigeonPresetTrip.HIGHWAY_TRIP -> PresetTrip.HIGHWAY_TRIP
+        PigeonPresetTrip.TRAIN_TRIP -> PresetTrip.TRAIN_TRIP
+        PigeonPresetTrip.BOAT_TRIP -> PresetTrip.BOAT_TRIP
+        PigeonPresetTrip.BUS_TRIP -> PresetTrip.BUS_TRIP
+        PigeonPresetTrip.TRIP_WITH_CRASH_CONFIRMED10KM_H -> PresetTrip.TRIP_WITH_CRASH_1(PresetTripCrash1.CONFIRMED_10KMH)
+        PigeonPresetTrip.TRIP_WITH_CRASH_CONFIRMED20KM_H -> PresetTrip.TRIP_WITH_CRASH_1(PresetTripCrash1.CONFIRMED_20KMH)
+        PigeonPresetTrip.TRIP_WITH_CRASH_CONFIRMED30KM_H -> PresetTrip.TRIP_WITH_CRASH_1(PresetTripCrash1.CONFIRMED_30KMH)
+        PigeonPresetTrip.TRIP_WITH_CRASH_UNCONFIRMED0KM_H -> PresetTrip.TRIP_WITH_CRASH_1(PresetTripCrash1.UNCONFIRMED_0KMH)
+        PigeonPresetTrip.TRIP_WITH_CRASH_CONFIRMED30KM_HSTILL_DRIVING -> PresetTrip.TRIP_WITH_CRASH_2_STILL_DRIVING
+    }
+}
