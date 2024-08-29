f39f553c8a28a64ef3c0f0a61c2446910e308388
Amine GAHBICHE <amine.gahbiche@drivequant.com>
2024-08-29 16:10:47 +0200

    chore: refer to the main flutter example SDK

--- packages/drivekit_driver_data/flutter_drivekit_driver_data/example/lib/main.dart
+++ packages/drivekit_driver_data/flutter_drivekit_driver_data/example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_drivekit_driver_data/flutter_drivekit_driver_data.dart';
 
 void main() => runApp(const MyApp());
 
@@ -24,46 +23,13 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('DriveKitDriverData Example')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () async {
-                const tripId = 'TRIP_ID_HERE';
-                final tripSyncResult =
-                    await DriveKitDriverData.instance.getTrip(tripId);
-                final alertMessage =
-                    tripSyncResult?.status == TripSyncStatus.success
-                        ? 'Trip ${tripSyncResult?.trip?.itinId} well received'
-                        : 'Trips not received';
-                if (context.mounted) {
-                  await showDialog<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Get Trip'),
-                        content: Text(alertMessage),
-                        actions: <Widget>[
-                          TextButton(
-                            child: const Text('OK'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-              },
-              child: const Text(
-                'Get Trip',
-              ),
-            ),
-          ],
-        ),
+      body: const Column(
+        children: [
+          Text('Please refer to the main flutter example SDK'),
+          Text(
+            'https://github.com/DriveQuantPublic/flutter-drivekit/tree/main/example',
+          ),
+        ],
       ),
     );
   }
